470effab25392adad66cda16421c2b3858e13c03
eakmotion <eakpun@gmail.com>
2017-01-06 13:29:40 +0700

    Fix on missing commas in data_seeder

--- data_seeder.py
+++ data_seeder.py
@@ -9,7 +9,7 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
-user1 = User(name="Eak Pun", email="eakpun@gmail.com",
+user1 = User(name="Eak Mock", email="eakmock@gmail.com",
              picture='http://lh3.googleusercontent.com/-W7N4DI18qcc/AAAAAAAAAAI/AAAAAAAAAAA/AKB_U8vFWNOxOUc5EY3-_U0b1Mo8-We_ng/s192-c-mo/photo.jpg')
 session.add(user1)
 session.commit()
@@ -21,7 +21,7 @@ session.commit()
 item1 = Item(name="Baseball bat",
              description="A baseball",
              price="100",
-             picture="https://www.baseballsavings.com/wcsstore/CatalogAssetStore/Attachment/images/products/baseball/P101443/1-z.jpg"
+             picture="https://www.baseballsavings.com/wcsstore/CatalogAssetStore/Attachment/images/products/baseball/P101443/1-z.jpg",
              category=cat1, user=user1)
 session.add(item1)
 session.commit()
@@ -33,7 +33,7 @@ session.commit()
 item2 = Item(name="Football",
              description="A large premium footbal.",
              price="59",
-             picture="https://n2.sdlcdn.com/imgs/b/e/o/Nike-Football-SDL771347869-1-56a7c.jpg"
+             picture="https://n2.sdlcdn.com/imgs/b/e/o/Nike-Football-SDL771347869-1-56a7c.jpg",
              category=cat2, user=user1)
 session.add(item2)
 session.commit()
