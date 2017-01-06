c182e56e567081c531f114a296df6de89a97856f
eakmotion <eakpun@gmail.com>
2017-01-06 13:41:06 +0700

    Fix on missing createUser method

--- main.py
+++ main.py
@@ -263,6 +263,14 @@ def getUserID(email):
     except:
         return None
 
+def createUser(login_session):
+    newUser = User(name=login_session['username'], email=login_session[
+                   'email'], picture=login_session['picture'])
+    session.add(newUser)
+    session.commit()
+    user = session.query(User).filter_by(email=login_session['email']).one()
+    return user.id
+
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
     app.debug = True
